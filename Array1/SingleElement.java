e41d642d19bd10fe212934d5604bfa720fb911ce
iamvansh <vanshranawat48@gmail.com>
2025-09-23 19:15:33 +0530

    add SingleElement

---
+++ Array1/SingleElement.java
@@ -0,0 +1,26 @@
+package Array1;
+
+public class SingleElement {
+    public static void singleElement(int nums[]){
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            largest = Math.max(largest, nums[i]);
+        }
+        int count[] = new int[largest+1];
+        for(int i=0;i<nums.length;i++){
+            count[nums[i]]++;
+        }
+        int element=-1;
+        for(int i=0;i<count.length;i++){
+            if (count[i]==1) {
+                element= i;
+                break;
+            }
+        }
+        System.out.println("the single element is:"+element);
+    }
+    public static void main(String args[]){
+        int nums[]={1};
+        singleElement(nums);
+    }
+}
