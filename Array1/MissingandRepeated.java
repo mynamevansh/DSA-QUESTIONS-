48af66779186d0dd0e522db5f2774c13eade31ff
iamvansh <vanshranawat48@gmail.com>
2025-09-24 16:42:28 +0530

    add MissingandRepeated

---
+++ Array1/MissingandRepeated.java
@@ -0,0 +1,33 @@
+public class MissingandRepeated {
+    public static int[] missingandrepeated(int grid[][]){
+        int n = grid.length;
+        int N = n*n;
+        boolean seen[] = new boolean[N+1];
+        int repeat = -1;
+        int missing = -1;
+
+        for(int row[]:grid){
+            for(int element:row){
+                if (seen[element]) {
+                    repeat = element;
+                }
+                else{
+                    seen[element]=true;
+                }
+            }
+        }
+        for(int i=1;i<=N;i++){
+            if(!seen[i]){
+                missing=i;
+                break;
+            }
+        }
+        return new int[]{repeat,missing};
+    }
+    public static void main (String args[]){
+        int grid[][] = {{1,3},
+                        {3,4}};
+        int result[] = missingandrepeated(grid);
+        System.out.println("Repeated: " + result[0] + ", Missing: " + result[1]);
+    }
+}
