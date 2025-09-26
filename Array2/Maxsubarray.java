ebc41e817e4227883717383fe4e6afacb1dbd865
Vansh Pratap Singh Ranawat <162921078+mynamevansh@users.noreply.github.com>
2025-09-26 16:18:49 +0530

    Improve maxSubArray method for negative elements
    Fix maxSubArray method to handle negative numbers correctly.

--- Array2/Maxsubarray.java
+++ Array2/Maxsubarray.java
@@ -19,3 +19,23 @@ public class Maxsubarray {
         System.out.println(maxSubArray(nums));
     }
 }
+------------------------------------------------------------
+(but what happen if all the elements in the array are negative????)
+
+package Array2;
+
+public class Maxsubarray {
+    public static int maxSubArray(int[] nums){
+        int sum = nums[0];
+        int max = nums[0];
+        for(int i=0;i<nums.length;i++){
+            sum=Math.max(nums[i],sum+nums[i]);
+            max=Math.max(max, sum);
+        }
+        return max;
+    }
+    public static void main(String args[]){
+        int [] nums={-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+}
