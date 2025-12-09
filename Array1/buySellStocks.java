efca83acda613debc91c904f7bdb7881dc2c7349
iamvansh <vanshranawat48@gmail.com>
2025-12-09 16:13:24 +0530

    add optimal solution

--- Array1/buySellStocks.java
+++ Array1/buySellStocks.java
@@ -3,25 +3,20 @@
 
 import java.util.*;
 public class buySellStocks{
-    public static void BuySellStocks(int prices[]){
-        int costprice[] = new int[prices.length];
-        costprice[0] = prices[0];
-
-        for(int i=1;i<prices.length;i++){
-            costprice[i] = Math.min(prices[i],costprice[i-1]); 
+    public static int BuySellStocks(int prices[]){
+        int minprice=prices[0];
+        int maxprofit=0;
+        for(int price:prices){
+            minprice=Math.min(minprice,price);
+            maxprofit=Math.max(maxprofit,price-minprice);
         }
-        int maxprofit = Integer.MIN_VALUE;
-        for(int i=0;i<prices.length;i++){
-            int profit = prices[i] - costprice[i];
-            
-            maxprofit = Math.max(maxprofit,profit);
-        }
-        System.out.println("maxprofit is:"+maxprofit);
+        return maxprofit;
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int prices[] = {7,6,4,3,1}; 
-        BuySellStocks(prices);
+        int maxprofit=BuySellStocks(prices);
+        System.out.println("maxprofit is:"+maxprofit);
         sc.close();
     }
 }
