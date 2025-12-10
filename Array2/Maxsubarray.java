1fd12d40b5aab39435a3b68c7bb73a0eb13d06c8
iamvansh <vanshranawat48@gmail.com>
2025-12-10 11:46:32 +0530

    add optimal solution

--- Array2/Maxsubarray.java
+++ Array2/Maxsubarray.java
@@ -3,11 +3,15 @@
 package Array2;
 public class Maxsubarray {
     public static int maxSubArray(int[] nums){
-        int sum = nums[0];
-        int max = nums[0];
+        int sum=0;
+        int max=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
-            sum=Math.max(nums[i],sum+nums[i]);
-            max=Math.max(max, sum);
+            if (sum<0) {
+                sum=0;
+            }
+            sum+=nums[i];
+            max=Math.max(max,sum);
+            
         }
         return max;
     }
@@ -16,26 +20,3 @@ public class Maxsubarray {
         System.out.println(maxSubArray(nums));
     }
 }
-//---------------------------------------------------------------------------
-//(but what happen if all the elements in the array are negative????)
-
-/* 
-package Array2;
-
-public class axsubarray {
-    public static int maxSubArray(int[] nums){
-        int sum = nums[0];
-        int max = nums[0];
-        for(int i=0;i<nums.length;i++){
-            sum=Math.max(nums[i],sum+nums[i]);
-            max=Math.max(max, sum);
-        }
-        return max;
-    }
-    public static void main(String args[]){
-        int [] nums={-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println(maxSubArray(nums));
-    }
-}
-
-*/
