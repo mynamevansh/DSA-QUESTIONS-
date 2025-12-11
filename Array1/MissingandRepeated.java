5363d2820a3dbfa3c44d31c6ef2343ae7fc46861
iamvansh <vanshranawat48@gmail.com>
2025-12-11 10:09:07 +0530

    add the optimal solution

--- Array1/MissingandRepeated.java
+++ Array1/MissingandRepeated.java
@@ -4,27 +4,25 @@ import java.util.HashMap;
 
 public class MissingandRepeated {
     public static int[] missingandrepeated(int grid[][]){
-        HashMap<Integer,Boolean> map = new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         int repeated = -1;
         int missing = -1;
         int n = grid.length;
-        for(int[] num : grid){
-            for(int element : num){
-                if (map.containsKey(element)) {
-                    repeated = element;
-                }
-                else{
-                    map.put(element, true);
-                }
+        for(int[] nums:grid){
+            for(int num:nums){
+                map.put(num,map.getOrDefault(num, 0)+1);
             }
         }
         for(int i=1;i<=n*n;i++){
-            if (!map.containsKey(i)) {
-                missing = i;
-                break;
+            int count=map.getOrDefault(i,0);
+            if (count==0) {
+                missing=i;
+            }
+            if (count>1) {
+                repeated=i;
             }
         }
-        return new int[] {repeated ,missing};
+        return new int[] {repeated,missing};
         
     }
     public static void main (String args[]){
