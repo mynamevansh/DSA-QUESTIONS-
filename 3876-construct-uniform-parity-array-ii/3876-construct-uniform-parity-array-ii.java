2b329cad7b73d59304dd660ae95e745faa1e12e2
Vansh Pratap Singh Ranawat <162921078+mynamevansh@users.noreply.github.com>
2026-09-03 10:33:22 +0530

    Time: 2 ms (100%), Space: 121.1 MB (86.23%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n=nums1.length;
-        
-
-        Arrays.sort(nums1);
-        int smallest=nums1[0];
-        
-        
-        if(smallest%2==0){
-            for(int i=1;i<n;i++){
+        int min=Integer.MAX_VALUE;
+        for(int num:nums1){
+            min=Math.min(min,num);
+        }
+        if(min%2==0){
+            for(int i=0;i<nums1.length;i++){
                 if(nums1[i]%2!=0){
                     return false;
                 }
